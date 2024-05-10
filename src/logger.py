5c5dcd02a01154e5bc987a23d56332751c033857
John Sellers <sellers.e.john@gmail.com>
2024-05-10 16:53:35 -0400

    Modified code to not create an extra folder in the logs directory

--- src/logger.py
+++ src/logger.py
@@ -5,19 +5,17 @@ from datetime import datetime
 # Generating a log file name based on the current date and time
 log_file = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
 
-# Creating a path for the log file in a 'logs' directory within the current working directory
-logs_path = os.path.join(os.getcwd(), "logs", log_file)
-
 # Creating the 'logs' directory (if it doesn't exist) to store log files
 # exist_ok=True ensures no error is raised if the directory already exists
+logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
-# Creating the full path to the log file by joining the log directory path and the log file name
+# Creating the full path to the log file within the 'logs' directory
 log_file_path = os.path.join(logs_path, log_file)
 
 # Configuring the logging system with the specified parameters
 logging.basicConfig(
-    # Specifying the file where log messages will be written (the dynamically generated log file)
+    # Specifying the file where log messages will be written (inside the 'logs' directory)
     filename=log_file_path,
     
     # Defining the format of log messages using placeholders
