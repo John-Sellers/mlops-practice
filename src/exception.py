942fd46765d0467e5afa4f878044ee8a453db6b3
John Sellers <sellers.e.john@gmail.com>
2024-05-09 20:34:22 -0400

    Created CustomException class

--- src/exception.py
+++ src/exception.py
@@ -3,7 +3,7 @@ import sys
 
 # Define a function named error_message_detail that takes two parameters: error and error_detail
 def error_message_detail(error, error_detail):
-    
+
     # Extract the traceback information from the error_detail using sys.exc_info()
     _, _, exc_tb = error_detail.exc_info()
 
@@ -15,3 +15,20 @@ def error_message_detail(error, error_detail):
     
     # Return the constructed error message
     return error_message
+
+# Define a custom exception class named CustomException that inherits from Exception
+class CustomException(Exception):
+
+    # Define the initialization method (__init__) for the custom exception class
+    def __init__(self, error_message, error_detail):
+
+        # Call the initialization method of the parent class (Exception) using super()
+        super().__init__(error_message)
+        
+        # Assign the error message returned by error_message_detail() to self.error_message
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    # Define the string representation method (__str__) for the custom exception class
+    def __str__(self):
+        # Return the error message when the exception is converted to a string
+        return self.error_message
